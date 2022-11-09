c7e71d611e8a14cf970ee5643831590b8eb4369f
Brandon Ye <brandonye@lawn-128-61-45-126.lawn.gatech.edu>
2022-11-08 19:42:18 -0500

    Club application creator

---
+++ client/indeed-clone/src/components/reviewPosting.js
@@ -0,0 +1,9 @@
+const ReviewPosting = () => {
+    return (
+        <div className="reviewPosting">
+            <h1>Review Club Posting</h1>
+        </div>
+    )
+ }
+ export default ReviewPosting
+ 
