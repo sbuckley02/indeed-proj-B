425191ffbcf00f6a684d753485f60168d59b0766
Aria Sharma <ariasharmagt@gmail.com>
2022-11-15 19:18:57 -0500

    changes1

--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@
 const express = require('express');
 const cors = require('cors');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:webd3vIndeed8rojB@indeedbcluster.sp9r8xb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+	const collection = client.db("test").collection("devices");
+	// perform actions on the collection object
+	client.close();
+});
+
 // Create the Express app
 const app = express();
 const PORT = 4000;
@@ -18,10 +27,11 @@ app.use(cors());
 // A sample API endpoint - a GET request that returns some text
 // when you make a request at .../api/sample-req
 app.get("/api/sample-req", (req, res) => {
-	res.send('hehe');
+	res.send('this is a sample request');
 });
 
 // Start the server
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`)
 });
+
