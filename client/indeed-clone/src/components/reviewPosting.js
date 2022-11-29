b55c16d68fd338ebfc78b51272b86a49372f02fe
Brandon Ye <brandonye@lawn-128-61-40-140.lawn.gatech.edu>
2022-11-29 00:05:26 -0500

    yo

--- client/indeed-clone/src/components/reviewPosting.js
+++
@@ -1,9 +0,0 @@
-const ReviewPosting = () => {
-    return (
-        <div className="reviewPosting">
-            <h1>Review Club Posting</h1>
-        </div>
-    )
- }
- export default ReviewPosting
- 
